d8514a8a6ece8de5d3940677565c3de4c7580e19
Jesufemi-A <jesufemia1@gmail.com>
2024-07-14 13:44:02 -0700

    initial commit

---
+++ src/main/java/com/hng_s2_userauth/HngS2UserauthApplication.java
@@ -0,0 +1,13 @@
+package com.hng_s2_userauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HngS2UserauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HngS2UserauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hng_s2_userauth/HngS2UserauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hng_s2_userauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HngS2UserauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
