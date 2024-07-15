52163d03e81d70069ed256c156d82f3297af5a96
Jesufemi-A <jesufemia1@gmail.com>
2024-07-14 19:19:29 -0700

    dto to get record of user

---
+++ src/main/java/com/hng_s2_userauth/dto/UserRecordDto.java
@@ -0,0 +1,33 @@
+package com.hng_s2_userauth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+public class UserRecordDto {
+
+
+
+    private String status;
+    private String message;
+    private Data data;
+
+
+    @lombok.Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static  class Data{
+        private String userId;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String phone;
+
+
+    }
+
+
+}
