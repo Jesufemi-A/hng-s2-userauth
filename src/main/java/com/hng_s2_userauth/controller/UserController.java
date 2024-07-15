6f9e99cfc6158c394f5a089bcacadc748fad341e
Jesufemi-A <jesufemia1@gmail.com>
2024-07-14 19:29:10 -0700

    added feature to retrieve user record from the same organisation

---
+++ src/main/java/com/hng_s2_userauth/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.hng_s2_userauth.controller;
+
+
+import com.hng_s2_userauth.model.UserEntity;
+import com.hng_s2_userauth.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+
+    final UserService userService;
+
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<?> getUserRecord(@PathVariable String id, @AuthenticationPrincipal UserEntity user) {
+
+        return userService.getUserRecord(id, user);
+
+    }
+
+
+}
+
