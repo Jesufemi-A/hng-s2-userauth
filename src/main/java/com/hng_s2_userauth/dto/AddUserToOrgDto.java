8d3003eba84e0ffd8110f5c1a431acee53cda407
Jesufemi-A <jesufemia1@gmail.com>
2024-07-14 18:02:57 -0700

    added dto response for adding user to organisation

---
+++ src/main/java/com/hng_s2_userauth/dto/AddUserToOrgDto.java
@@ -0,0 +1,13 @@
+package com.hng_s2_userauth.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AddUserToOrgDto {
+
+    private String status;
+    private String message;
+}
